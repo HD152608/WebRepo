336846028f39d785f6083a5ceb84098699f857bb
덮어라.pc <.pc@DESKTOP-MB6A5SV>
2017-09-26 12:27:26 +0900

    1

--- WebClass/src/org/dimigo/servlet/LoginServlet.java
+++ WebClass/src/org/dimigo/servlet/LoginServlet.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.SessionCookieConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,86 +16,84 @@ import org.dimigo.vo.UserVO;
 import org.json.simple.JSONObject;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 
 /**
  * Servlet implementation class LoginServlet
  */
-@WebServlet(name = "login", urlPatterns = { "/login" })
+@WebServlet("/login")
+
 public class LoginServlet extends HttpServlet {
    private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
    /**
-    * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+    * @see HttpServlet#HttpServlet()
     */
-   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-      // TODO Auto-generated method stub
-	   RequestDispatcher rd = request.getRequestDispatcher("/jsp/login.jsp");
-	   rd.forward(request, response);
-//     response.getWriter().append("Served at: ").append(request.getContextPath());
+   public LoginServlet() {
+      super();
+      // TODO Auto-generated constructor stub
    }
 
    /**
-    * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+    * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+    *      response)
     */
-   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-      response.setContentType("application/json;charset=utf-8");
+   protected void doGet(HttpServletRequest request, HttpServletResponse response)
+         throws ServletException, IOException {
+      RequestDispatcher rd = request.getRequestDispatcher("/jsp/login.jsp");
+      rd.forward(request, response);
+   }
+
+   /**
+    * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+    *      response)
+    */
+   protected void doPost(HttpServletRequest request, HttpServletResponse response)
+         throws ServletException, IOException {
+      response.setContentType("text/html;charset=utf-8");
       PrintWriter out = response.getWriter();
-      
+
       request.setCharacterEncoding("utf-8");
       String id = request.getParameter("id");
       String pwd = request.getParameter("pwd");
-      System.out.printf("id : %s, pwd : %s\n",id, pwd);
-      
-      
-//      out.println("{");
-//      out.println("\"id\" : " +"\""+ id+"\"");
-//      out.println("}");
-      
-     Gson gson = new Gson();
-     JsonObject object = new JsonObject();
-     object.addProperty("id", id);
-     
-    		 
+      System.out.printf("id : %s, pwd : %s", id, pwd);
+
+      boolean result = true;
+      if (result) {
+         // 세션에 사용자 생성
+         HttpSession session = request.getSession();
+         UserVO user = new UserVO();
+         user.setId(id);
+         user.setName("홍길동");
+         user.setNickname("의적");
+
+         session.setAttribute("user", user);
+         
+         RequestDispatcher rd = request.getRequestDispatcher("jsp/home.jsp");
+         rd.forward(request, response);
+      }
       out.close();
    }
-   
-   protected void doPost2(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	      response.setContentType("text/html;charset=utf-8");
-	      PrintWriter out = response.getWriter();
-	      
-	      request.setCharacterEncoding("utf-8");
-	      String id = request.getParameter("id");
-	      String pwd = request.getParameter("pwd");
-	      System.out.printf("id : %s, pwd : %s\n",id, pwd);
-	      
-	      // id, pwd 사용자 인증 체크
-	      boolean result = true;
-	      
-	      if(result){
-	    	  // 세션에 사용자 정보를 생성하여 담기
-	    	  HttpSession session= request.getSession();
-	    	  UserVO user = new UserVO();
-	    	  user.setId(id);
-	    	  user.setName("홍길동");
-	    	  user.setNickname("의적");
-	    	  
-	    	  session.setAttribute("user", user);
-	    	  RequestDispatcher rd = request.getRequestDispatcher("/jsp/home.jsp");
-	   	      rd.forward(request, response);
-	      }
-	      
-	    		 
-	      out.close();
-	   }
 
+   protected void doPost2(HttpServletRequest request, HttpServletResponse response)
+         throws ServletException, IOException {
+      // TODO Auto-generated method stub
+      response.setContentType("application/json;charset=utf-8");
+      PrintWriter out = response.getWriter();
+
+      String id = request.getParameter("id");
+      String pwd = request.getParameter("pwd");
+
+      JsonObject jo = new JsonObject();
+
+      jo.addProperty("id", id);
+
+      out.write(jo.toString());
+
+      System.out.println(jo.toString());
+
+      out.close();
+   }
 
 }
